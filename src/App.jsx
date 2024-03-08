9ed9235a6aa208318a0afc6413cfd350bc0e793a
Juan P Javier <juanpablojavier14@gmail.com>
2024-03-08 04:47:43 -0300

    miniTool footer in production to see information

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Products } from "./components/Products";
 import { products as initialProducts } from "./mock/products.json";
 import "./App.css";
 import { useState, createContext } from "react";
+import { Footer } from "./components/Footer";
+import { IS_DEVELOPMENT } from "./config";
 
 function useFilters (){
   const [filters, setFilters] = useState({
@@ -19,18 +21,19 @@ function useFilters (){
     });
   };
 
-  return {setFilters, filterProducts}
+  return {filters, setFilters, filterProducts}
 }
 
 function App() {
   const [products] = useState(initialProducts);
-  const {filterProducts, setFilters} = useFilters()
+  const {filters, setFilters, filterProducts} = useFilters()
   const filteredProducts = filterProducts(products)
 
   return (
     <>
       <Header changeFilters={setFilters}/>
       <Products products={filteredProducts} />
+      { IS_DEVELOPMENT && <Footer filters={filters}/>}
     </>
   );
 }
